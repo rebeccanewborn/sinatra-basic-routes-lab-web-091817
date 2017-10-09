5c764375197a5462cd107253150b8acd5361f51c
rebeccanewborn <rjn58@cornell.edu>
2017-10-08 21:32:14 -0400

    lb

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Becky"
+  end
+
+  get '/hometown' do
+    "My hometown is Chappaqua, NY"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Midnight in Harlem"
+  end
+
 end
